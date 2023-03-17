1ff9ce425794880ec2ff8600fb2f1df1b68eef24
Igor de Paula <igordepaula1@gmail.com>
2023-03-17 16:18:10 +0000

    Updates DuffelCheckout to use ErrorBoundary

--- src/component/DuffelCheckout.tsx
+++ src/component/DuffelCheckout.tsx
@@ -1,44 +1,51 @@
 import * as React from "react";
 import { makeMockPayloadForCreateOrder } from "../lib/makeMockPayloadForCreateOrder";
 import { DuffelAPI } from "../types/DuffelAPI";
+import { ErrorBoundary } from "./ErrorBoundary";
+import { TestSentry } from "./TestSentry";
 
 export interface DuffelCheckoutProps {
   offer: DuffelAPI.Offer;
   onPayloadReady: (data: DuffelAPI.CreateOrderPayload) => void;
 }
 
-export const DuffelCheckout: React.FC<DuffelCheckoutProps> = ({
-  offer,
-  onPayloadReady,
-}) => {
-  const [mountTime] = React.useState(new Date());
-  const mockOrder = makeMockPayloadForCreateOrder(offer.id);
-  const serialisedOfferToRender = JSON.stringify(offer, null, 2);
-  return (
-    <div>
-      <pre
-        style={{
-          padding: "12px",
-          color: "white",
-          backgroundColor: "#0A0A0A",
-        }}
-      >
-        {serialisedOfferToRender}
-      </pre>
+export const DuffelCheckoutWithoutErrorBoundary: React.FC<DuffelCheckoutProps> =
+  ({ offer, onPayloadReady }) => {
+    const [mountTime] = React.useState(new Date());
+    const mockOrder = makeMockPayloadForCreateOrder(offer.id);
+    const serialisedOfferToRender = JSON.stringify(offer, null, 2);
 
-      <button onClick={() => onPayloadReady(mockOrder)}>
-        Call{" "}
-        <b>
-          <code>onPayloadReady</code>
-        </b>
-      </button>
+    return (
+      <div>
+        <pre
+          style={{
+            padding: "12px",
+            color: "white",
+            backgroundColor: "#0A0A0A",
+          }}
+        >
+          {serialisedOfferToRender}
+        </pre>
 
-      <p>
-        Component mounted on:{" "}
-        <b>
-          <code>{mountTime.toISOString()}</code>
-        </b>
-      </p>
-    </div>
-  );
-};
+        <button onClick={() => onPayloadReady(mockOrder)}>
+          Call{" "}
+          <b>
+            <code>onPayloadReady</code>
+          </b>
+        </button>
+
+        <p>
+          Component mounted on:{" "}
+          <b>
+            <code>{mountTime.toISOString()}</code>
+          </b>
+        </p>
+      </div>
+    );
+  };
+
+export const DuffelCheckout: React.FC<DuffelCheckoutProps> = (props) => (
+  <ErrorBoundary>
+    <DuffelCheckoutWithoutErrorBoundary {...props} />
+  </ErrorBoundary>
+);
