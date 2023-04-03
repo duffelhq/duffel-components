86780a46e0e86559257979e05215842f18b98521
Igor de Paula <igordepaula1@gmail.com>
2023-04-03 14:50:06 +0100

    Updates card design to match figma
    - https://www.figma.com/file/KjfQGqvdiQnCJftxxeDar9/Ancillaries?node-id=75-11366&t=Yt2RcNaHRMPczNCc-11

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,63 +1,44 @@
 import { Button } from "./Button";
 import { Icon, IconName } from "./Icon";
-import { Stamp } from "./Stamp";
 
 export interface CardProps {
   title: string;
   icon: IconName;
-  statusTag: "added" | "not-added" | "not-available";
-  onClick: () => void;
+  onClick?: (() => void) | null;
   children: React.ReactNode;
+  ctaLabel: string;
 }
 
 export const Card: React.FC<CardProps> = ({
   title,
   icon,
-  statusTag,
   onClick,
   children,
+  ctaLabel,
 }) => (
   <div
     style={{
       background: "transparent",
       border: "solid 1px rgba(226, 226, 232, 1)",
-      display: "grid",
-      gridTemplateColumns: "26px 1fr 100px",
+      display: "flex",
       rowGap: "20px",
-      columnGap: "20px",
       padding: "20px",
       borderRadius: "8px",
+      justifyContent: "space-between",
     }}
   >
-    <div>
-      <Icon name={icon} />
-    </div>
-    <div
+    <p
       style={{
         textAlign: "start",
-        fontWeight: "600",
-        fontSize: "16px",
-        lineHeight: "24px",
+        display: "flex",
+        alignItems: "center",
+        columnGap: "8px",
       }}
+      className="p1--semibold"
     >
+      <Icon name={icon} />
       {title}
-    </div>
-    {statusTag === "added" && (
-      <Stamp color="#092B00" backgroundColor="#F6FFED">
-        Added
-      </Stamp>
-    )}
-    {statusTag === "not-added" && (
-      <Stamp backgroundColor="#F6F5F9" color="#4B4B55">
-        Not added
-      </Stamp>
-    )}
-    {statusTag === "not-available" && (
-      <Stamp backgroundColor="#F6F5F9" color="#4B4B55">
-        Not available
-      </Stamp>
-    )}
-    <div />
+    </p>
     <div
       style={{
         textAlign: "start",
@@ -65,17 +46,22 @@ export const Card: React.FC<CardProps> = ({
         fontSize: "16px",
         lineHeight: "24px",
         color: "#696972",
+        display: "flex",
+        justifyContent: "end",
+        alignItems: "center",
       }}
     >
       {children}
-    </div>
-    <div style={{ display: "flex", justifyContent: "end" }}>
-      <Button
-        text="Add"
-        intent="PRIMARY"
-        onClick={onClick}
-        disabled={statusTag === "not-available"}
-      />
+      <div style={{ marginLeft: "12px" }}>
+        {onClick !== undefined && (
+          <Button
+            text={ctaLabel}
+            intent="PRIMARY"
+            onClick={onClick || undefined}
+            disabled={onClick === null}
+          />
+        )}
+      </div>
     </div>
   </div>
 );
