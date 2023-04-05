b31baa5b07aaedaad236686e8d8b893a35ce8396
Igor de Paula <igordepaula1@gmail.com>
2023-04-05 14:26:22 +0100

    Update the BaggageSelectionModal interface to return selected services on close

--- src/components/BaggageSelectionModal.tsx
+++ src/components/BaggageSelectionModal.tsx
@@ -8,35 +8,46 @@ import { BaggageSelectionModalBody } from "./BaggageSelectionModalBody";
 import { BaggageSelectionModalFooter } from "./BaggageSelectionModalFooter";
 import { BaggageSelectionModalHeader } from "./BaggageSelectionModalHeader";
 import { Modal } from "./Modal";
+import { getPassengerMapById } from "@lib/getPassengerMapById";
 
 export interface BaggageSelectionModalProps {
   offer: Offer;
   passengers: CreateOrderPayload["passengers"];
   selectedServices: CreateOrderPayloadServices;
-  onClose: () => void;
-  onSubmit: (selectedServices: CreateOrderPayloadServices) => void;
+  onClose: (selectedServices: CreateOrderPayloadServices) => void;
 }
 
 export const BaggageSelectionModal: React.FC<BaggageSelectionModalProps> = ({
   offer,
+  passengers,
   onClose,
+  selectedServices,
 }) => {
+  const [currentSegmentIndex, setCurrentSegmentIndex] = useState(0);
+
   const segments = offer.slices.reduce(
-    (accumulator, slice) => [...slice.segments, ...accumulator],
+    (accumulator, slice) => [...accumulator, ...slice.segments],
     new Array<OfferSliceSegment>()
   );
 
-  const [currentSegmentIndex, setCurrentSegmentIndex] = useState(0);
+  const [selectedServicesState, setSelectedServicesState] =
+    React.useState(selectedServices);
 
   return (
-    <Modal onClose={onClose}>
+    <Modal onClose={() => onClose(selectedServicesState)}>
       <BaggageSelectionModalHeader
         segmentCount={segments.length}
         currentSegment={segments[currentSegmentIndex]}
         currentSegmentIndex={currentSegmentIndex}
         setCurrentSegmentIndex={(index) => setCurrentSegmentIndex(index)}
       />
-      <BaggageSelectionModalBody offer={offer} />
+      <BaggageSelectionModalBody
+        selectedServices={selectedServicesState}
+        offer={offer}
+        passengersById={getPassengerMapById(passengers)}
+        segment={segments[currentSegmentIndex]}
+        setSelectedServices={setSelectedServicesState}
+      />
       <BaggageSelectionModalFooter offer={offer} />
     </Modal>
   );
