4ebc35caf1f486e87644292361d0495efd9a963a
Igor de Paula <igordepaula1@gmail.com>
2023-04-06 11:19:53 +0100

    Creates new baggage footer

--- src/components/BaggageSelectionModalFooter.tsx
+++ src/components/BaggageSelectionModalFooter.tsx
@@ -1,11 +1,75 @@
-import { Offer } from "../types/Offer";
+import { moneyStringFormatter } from "@lib/formatConvertedCurrency";
+import { ServicePriceMapById } from "@lib/getServicePriceMapById";
+import { getTotalAmountForServicesWithPriceMap } from "@lib/getTotalAmountForServices";
+import { getTotalQuantity } from "@lib/getTotalQuantity";
+import { withPlural } from "@lib/withPlural";
 import React from "react";
+import { CreateOrderPayloadServices } from "../types/CreateOrderPayload";
+import { Button } from "./Button";
 
 export interface BaggageSelectionModalFooterProps {
-  // TODO
-  offer: Offer;
+  currency: string;
+  selectedServices: CreateOrderPayloadServices;
+  servicePrices: ServicePriceMapById;
+
+  isFirstSegment: boolean;
+  isLastSegment: boolean;
+
+  onNextSegmentButtonClicked: () => void;
+  onPreviousSegmentButtonClicked: () => void;
+  onClose: () => void;
 }
 
 export const BaggageSelectionModalFooter: React.FC<
   BaggageSelectionModalFooterProps
-> = () => <div style={{ padding: "24px" }}>BaggageSelectionModalFooter</div>;
+> = ({
+  selectedServices,
+  servicePrices,
+  currency,
+  isFirstSegment,
+  isLastSegment,
+  onNextSegmentButtonClicked,
+  onPreviousSegmentButtonClicked,
+  onClose,
+}) => {
+  const totalQuantity = getTotalQuantity(selectedServices);
+  const totalAmount = getTotalAmountForServicesWithPriceMap(
+    servicePrices,
+    selectedServices
+  );
+  const totalAmountLabel = moneyStringFormatter(currency)(totalAmount);
+
+  return (
+    <div style={{ padding: "16px 24px 24px" }}>
+      <div className="flex--space-between">
+        <div>
+          Price for {withPlural(totalQuantity, "extra bag", "extra bags")}
+        </div>
+        <div className="h3--semibold">+ {totalAmountLabel}</div>
+      </div>
+
+      <div
+        style={{
+          marginTop: "16px",
+          display: "grid",
+          columnGap: "12px",
+          gridTemplateColumns: "repeat(2, 1fr)",
+        }}
+      >
+        <Button
+          text="Back"
+          disabled={isFirstSegment}
+          intent="MUTED"
+          onClick={() => onPreviousSegmentButtonClicked()}
+        />
+        <Button
+          text={isLastSegment ? "Done" : "Next"}
+          intent="PRIMARY"
+          onClick={() =>
+            isLastSegment ? onClose() : onNextSegmentButtonClicked()
+          }
+        />
+      </div>
+    </div>
+  );
+};
