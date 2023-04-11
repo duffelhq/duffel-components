5b7518819b0cf4076f146c6029da1d69e91cc9de
Igor de Paula <igordepaula1@gmail.com>
2023-04-11 14:07:07 +0100

    Updates upload script to upload mocks

--- scripts/upload-current-version.sh
+++ scripts/upload-current-version.sh
@@ -7,7 +7,13 @@ VERSION=$(jq '.version' package.json -r)
 
 gcloud storage rm -r "gs://duffel-assets/ancillaries-component/$VERSION"
 
+# javascript
 gcloud storage cp dist/components/DuffelCheckoutCustomElement.js "gs://duffel-assets/ancillaries-component/$VERSION/DuffelCheckoutCustomElement.js"
 gcloud storage cp dist/components/DuffelCheckoutCustomElement.js.map "gs://duffel-assets/ancillaries-component/$VERSION/DuffelCheckoutCustomElement.js.map"
+
+# styles
 gcloud storage cp dist/styles/global.css "gs://duffel-assets/ancillaries-component/$VERSION/global.css"
 gcloud storage cp dist/styles/global.css.map "gs://duffel-assets/ancillaries-component/$VERSION/global.css.map"
+
+# mock data
+gcloud storage cp -r dist/mocks "gs://duffel-assets/ancillaries-component/$VERSION/mocks"
