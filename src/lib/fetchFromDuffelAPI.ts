95bdbd8940dc09c6ab0bc38d62487db2a39c1313
Igor de Paula <igordepaula1@gmail.com>
2023-04-14 15:41:05 +0100

    Adds user agent on requests to Duffel

--- src/lib/fetchFromDuffelAPI.ts
+++ src/lib/fetchFromDuffelAPI.ts
@@ -1,4 +1,5 @@
-const DUFFEL_API_URL = "https://localhost:4000";
+const DUFFEL_API_URL = process.env.DUFFEL_API_URL;
+const COMPONENT_VERSION = process.env.COMPONENT_VERSION;
 
 export async function fetchFromDuffelAPI(
   withClientKey: string,
@@ -14,6 +15,7 @@ export async function fetchFromDuffelAPI(
       headers: {
         "Duffel-Version": "v1",
         Authorization: `Bearer ${withClientKey}`,
+        "User-Agent": `Duffel/ancillaries-component/${COMPONENT_VERSION}`,
       },
     }
   );
