6bb4a1137412bc45a60c2dbbafd293706899feaa
Igor de Paula <igordepaula1@gmail.com>
2023-04-25 09:10:52 +0100

    Bonus: simplifies inspect component

--- src/components/Inspect.tsx
+++ src/components/Inspect.tsx
@@ -1,19 +1,9 @@
 interface InspectProps {
-  data: Record<string, any>;
+  props: Record<string, any>;
+  state: Record<string, any>;
 }
 
-export const Inspect: React.FC<InspectProps> = ({
-  data: {
-    offer_id,
-    client_key,
-    passengers,
-    baggageSelectedServices,
-    seatSelectedServices,
-    offer,
-    error,
-    seatMaps,
-  },
-}) => (
+export const Inspect: React.FC<InspectProps> = ({ props, state }) => (
   <pre
     style={{
       border: "solid 1px black",
@@ -22,30 +12,17 @@ export const Inspect: React.FC<InspectProps> = ({
     }}
   >
     <>
-      <b>{"Attributes:\n"}</b>
-      {`${offer_id ? "✓" : "x"} offer_id: ${offer_id}\n`}
-      {`${client_key ? "✓" : "x"} client_key: ${client_key}\n\n`}
+      <b>{"Properties:\n"}</b>
+      {Object.entries(props).map(
+        ([key, value]) =>
+          `${value ? "✓" : "x"} ${key}: ${JSON.stringify(value)}\n`
+      )}
 
-      <b>{"Init data:\n"}</b>
-      {`${passengers ? "✓" : "x"} passengers: ${JSON.stringify(
-        passengers
-      )}\n\n`}
-
-      <b>{"Internal state:\n"}</b>
-      {`${offer ? "✓" : "x"} offer: ${
-        JSON.stringify(offer) || (error ? "Failed" : "Loading...")
-      }\n`}
-      {`${seatMaps ? "✓" : "x"} seatMaps: ${
-        JSON.stringify(seatMaps) || (error ? "Failed" : "Loading...")
-      }\n`}
-      {`${
-        baggageSelectedServices ? "✓" : "x"
-      } baggageSelectedServices: ${JSON.stringify(baggageSelectedServices)}\n`}
-      {`${
-        seatSelectedServices ? "✓" : "x"
-      } seatSelectedServices: ${JSON.stringify(seatSelectedServices)}\n\n`}
-      <b>{"Error:\n"}</b>
-      {`${error ? "✓" : "x"} error: ${error}\n`}
+      <b>{"\nInternal state:\n"}</b>
+      {Object.entries(state).map(
+        ([key, value]) =>
+          `${value ? "✓" : "x"} ${key}: ${JSON.stringify(value)}\n`
+      )}
     </>
   </pre>
 );
