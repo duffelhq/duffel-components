080406cba5204a6864bc33d646a032463da816d1
Igor de Paula <igordepaula1@gmail.com>
2023-05-22 11:59:31 +0100

    Tests must include react. This makes sure that it throws the error we want

--- src/tests/DuffelAncillaries.test.tsx
+++ src/tests/DuffelAncillaries.test.tsx
@@ -1,26 +1,22 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-/**
- * In this file, we want to allow usage of @ts-ignore so
- * that we can test functions with invalid props.
- */
-
-import { render } from "@testing-library/react";
 import { DuffelAncillaries } from "@components/DuffelAncillaries";
+import { describe, expect, test } from "@jest/globals";
+import { render } from "@testing-library/react";
+import React from "react";
+
+const ExampleWithEmptyServices: React.FC = () => (
+  <DuffelAncillaries
+    onPayloadReady={jest.fn()}
+    passengers={[]}
+    services={[]}
+    offer_id="offer_id"
+    client_key="client_key"
+  />
+);
 
 describe("DuffelAncillaries", () => {
-  it("should throw an error when services is empty", () => {
-    // Suppress console.error messages as we're purposefully testing for it.
-    jest.spyOn(console, "error").mockImplementation(jest.fn);
-    expect(() => {
-      render(
-        <DuffelAncillaries
-          onPayloadReady={() => {}} // eslint-disable-line @typescript-eslint/no-empty-function
-          passengers={[]}
-          services={[]}
-          offer_id="offer_id"
-          client_key="client_key"
-        />
-      );
-    }).toThrowError();
+  test("should throw an error when services is empty", () => {
+    expect(() => render(<ExampleWithEmptyServices />)).toThrow(
+      'You must provide at least one service in the "services" prop. Valid services: ["bags", "seats"]'
+    );
   });
 });
