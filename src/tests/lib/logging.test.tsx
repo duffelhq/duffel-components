1a80a73638807999a127571f59ea83f484d55cf5
Igor de Paula <igordepaula1@gmail.com>
2023-06-14 17:35:11 +0100

    fix: updates log usage to support the update

--- src/tests/lib/logging.test.tsx
+++ src/tests/lib/logging.test.tsx
@@ -1,27 +1,27 @@
-import { initializeLogger } from "@lib/logging";
+import { initializeLogger, log, logGroup } from "../../lib/logging";
 
 const consoleSpies = {
-  log: jest.spyOn(console, "log"),
+  log: jest.spyOn(console, "info"),
   groupCollapsed: jest.spyOn(console, "groupCollapsed"),
   groupEnd: jest.spyOn(console, "groupEnd"),
 };
 
 describe("initializeLogger", () => {
   it("should return functions that do nothing when debugMode is false", () => {
-    const logger = initializeLogger(false);
-    logger.log("This should not be logged");
+    initializeLogger(false);
+    log("This should not be logged");
     expect(consoleSpies.log).not.toHaveBeenCalled();
   });
 
   it("should return functions that log a message when debugMode is true", () => {
-    const logger = initializeLogger(true);
-    logger.log("This should be logged");
+    initializeLogger(true);
+    log("This should be logged");
 
     // Twice because initializeLogger logs a message when it's called,
     // and then the message we're testing.
     expect(consoleSpies.log).toHaveBeenCalledTimes(2);
 
-    logger.logGroup("This should be logged", ["one", "two"]);
+    logGroup("This should be logged", ["one", "two"]);
     expect(consoleSpies.groupCollapsed).toHaveBeenCalledTimes(1);
     expect(consoleSpies.groupEnd).toHaveBeenCalledTimes(1);
 
