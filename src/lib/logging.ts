032e6353e44f4f7b0cab79ea13c252579aaf56d0
Igor de Paula <igordepaula1@gmail.com>
2023-06-14 17:34:02 +0100

    refactor: updates logger to rely on localstorage instead of react context
    This should give us the flexibility to import and use outside of react components

--- src/lib/logging.ts
+++ src/lib/logging.ts
@@ -1,4 +1,12 @@
-import { createContext, useContext } from "react";
+import * as Sentry from "@sentry/browser";
+
+const MESSAGE_PREFIX = "[Duffel Ancillaries] ";
+const LOCAL_STORAGE_KEY = "duffel-ancillaries-logger-state";
+
+const storeLoggerState = (shouldLog: boolean) =>
+  localStorage.setItem(LOCAL_STORAGE_KEY, shouldLog.toString());
+
+const shouldLog = () => localStorage.getItem(LOCAL_STORAGE_KEY) === "true";
 
 /**
  * The functions in this file are used to enable logging.
@@ -25,36 +33,30 @@ import { createContext, useContext } from "react";
  * log('This is a log message')
  * logGroup('These messages will be grouped together', ['This is a log message', 'This is another log message'])
  */
-
-const initializeLogger = (debugMode: boolean) => {
+export const initializeLogger = (debugMode: boolean): void => {
+  storeLoggerState(debugMode);
   if (debugMode) {
     log(
       `\n\nDebug mode is enabled. Information about your setup will be printed to the console.\n\nIf you do not want to enable debug mode (for example in a production environment), pass "debug: false" when initializing this component.\n\nLearn more about the Ancillaries component:\nhttp://duffel.com/docs/guides/ancillaries-component`
     );
   }
-
-  // We return functions that do nothing because it allows consumers
-  // of this function to not have to check if the logger is enabled or not.
-  // If we returned undefined, consumers would have to do something like:
-  // if (log) { log('message') }
-  //
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  const noop = () => {};
-
-  return {
-    log: debugMode ? log : noop,
-    logGroup: debugMode ? logGroup : noop,
-  };
 };
-
-const MESSAGE_PREFIX = "[Duffel Ancillaries] ";
 
 /**
  * Log a message to the console. Messages will be prefixed with "[Duffel Ancillaries]".
  * @param message The message to print to the console.
  */
-const log = (message: any) => {
-  console.log(MESSAGE_PREFIX, message);
+export const log = (message: any) => {
+  if (shouldLog()) {
+    // eslint-disable-next-line
+    console.info(MESSAGE_PREFIX, message);
+  } else {
+    Sentry.addBreadcrumb({
+      category: "Log message",
+      level: "info",
+      message,
+    });
+  }
 };
 
 /**
@@ -62,23 +64,24 @@ const log = (message: any) => {
  * @param groupName The name of the group of messages. This will be prefixed with "[Duffel Ancillaries]".
  * @param messages An array of messages to print to the console, inside the group.
  */
-function logGroup(groupName: string, messages: any[]): void;
+export function logGroup(groupName: string, messages: any[]): void;
 
 /**
  * Log a series of messages to the console inside a collapsible group.
  * @param groupName The name of the group of messages. This will be prefixed with "[Duffel Ancillaries]".
  * @param object An object to print to the console, inside the group.
  */
-function logGroup(groupName: string, object: { [key: string]: any }): void;
+export function logGroup(
+  groupName: string,
+  object: { [key: string]: any }
+): void;
 
 // Overloaded function implementation.
 // https://www.typescriptlang.org/docs/handbook/2/functions.html#function-overloads
-function logGroup(
+export function logGroup(
   groupName: string,
   messagesOrObject: any[] | { [key: string]: any }
 ): void {
-  console.groupCollapsed(MESSAGE_PREFIX, groupName);
-
   let transformedMessagesOrObject = [];
   if (Array.isArray(messagesOrObject)) {
     transformedMessagesOrObject = messagesOrObject;
@@ -87,14 +90,23 @@ function logGroup(
       ([key, value]) => ({ property: key, value })
     );
   }
-  transformedMessagesOrObject.forEach((message) => {
-    console.log(message);
-  });
 
-  console.groupEnd();
+  if (shouldLog()) {
+    // eslint-disable-next-line
+    console.groupCollapsed(MESSAGE_PREFIX, groupName);
+
+    transformedMessagesOrObject.forEach((message) => {
+      // eslint-disable-next-line
+      console.info(message);
+    });
+
+    // eslint-disable-next-line
+    console.groupEnd();
+  } else {
+    Sentry.addBreadcrumb({
+      category: `Log grouop: ${groupName}`,
+      level: "info",
+      data: transformedMessagesOrObject,
+    });
+  }
 }
-
-const LogContext = createContext(initializeLogger(false));
-const useLog = () => useContext(LogContext);
-
-export { LogContext, initializeLogger, useLog };
