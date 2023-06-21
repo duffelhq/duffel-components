450394e209050b6a92b667871504edd7246dfb29
Igor de Paula <igordepaula1@gmail.com>
2023-06-21 14:45:16 +0100

    fix: linter fix

--- src/examples/payments-just-typescript/src/index.ts
+++ src/examples/payments-just-typescript/src/index.ts
@@ -9,6 +9,10 @@ window.onload = () => {
     paymentIntentClientToken:
       "eyJjbGllbnRfc2VjcmV0IjoicGlfM0psczlVQWcySmhFeTh2WTBSTm1MU0JkX3NlY3JldF9QUW9yZXNuU3laeWJadGRiejZwNzBCbUdPIiwicHVibGlzaGFibGVfa2V5IjoicGtfdGVzdF9EQUJLY0E2Vzh6OTc0cTdPSWY0YmJ2MVQwMEpwRmMyOUpWIn0=",
   });
+
+  // eslint-disable-next-line
   onDuffelPaymentsFailedPayment((error) => console.error(error));
+
+  // eslint-disable-next-line
   onDuffelPaymentsSuccessfulPayment(() => console.log("Payment successful"));
 };
