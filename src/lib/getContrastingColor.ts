db3697399f669df190fe95689cd3189d96cfe90c
Igor de Paula <igordepaula1@gmail.com>
2023-06-26 18:35:50 +0100

    fix: test commit

--- src/lib/getContrastingColor.ts
+++ src/lib/getContrastingColor.ts
@@ -6,6 +6,7 @@ const getContrastingColor = (rgbString: string): "0,0,0" | "255,255,255" => {
   // Use relative-luminance to determine whether the color is dark enough or not.
   // See also: https://www.w3.org/WAI/GL/wiki/Relative_luminance
   const luminance = (0.2126 * red + 0.7152 * green + 0.0722 * blue) / 255;
+
   return luminance > 0.5 ? "0,0,0" : "255,255,255";
 };
 
