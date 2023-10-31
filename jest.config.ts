7519dea6c029eb99f146066ff73f79b64337eafc
Igor de Paula <igordepaula1@gmail.com>
2023-10-31 12:52:13 +0000

    removes storyshots

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "jest";
 const config: Config = {
   silent: true,
   testEnvironment: "jsdom",
-  roots: ["<rootDir>/src/tests", "<rootDir>/.storybook"],
+  roots: ["<rootDir>/src/tests"],
   moduleNameMapper: {
     "\\.css$": "<rootDir>/__mocks__/styleMock.js",
     "@lib/(.*)$": "<rootDir>/src/lib/$1",
