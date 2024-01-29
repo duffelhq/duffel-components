be3ead7b9d9d96aa3cc74d78d5bdd6d082eaeb28
Igor <igordepaula1@gmail.com>
2024-01-29 17:13:59 +0000

    Slice summary (#220)
    * updates vscode tools
    
    * Creates slice summary component

--- .yarn/sdks/prettier/bin/prettier.cjs
+++ .yarn/sdks/prettier/bin/prettier.cjs
@@ -4,17 +4,17 @@ const {existsSync} = require(`fs`);
 const {createRequire} = require(`module`);
 const {resolve} = require(`path`);
 
-const relPnpApiPath = "../../../.pnp.cjs";
+const relPnpApiPath = "../../../../.pnp.cjs";
 
 const absPnpApiPath = resolve(__dirname, relPnpApiPath);
 const absRequire = createRequire(absPnpApiPath);
 
 if (existsSync(absPnpApiPath)) {
   if (!process.versions.pnp) {
-    // Setup the environment to be able to require prettier/bin-prettier.js
+    // Setup the environment to be able to require prettier/bin/prettier.cjs
     require(absPnpApiPath).setup();
   }
 }
 
-// Defer to the real prettier/bin-prettier.js your application uses
-module.exports = absRequire(`prettier/bin-prettier.js`);
+// Defer to the real prettier/bin/prettier.cjs your application uses
+module.exports = absRequire(`prettier/bin/prettier.cjs`);
---
+++ src/components/DuffelNGSView/SliceSummary.tsx
@@ -0,0 +1,94 @@
+import { OfferSlice } from "@duffel/api/types";
+import { convertDurationToString } from "@lib/convertDurationToString";
+import { getTimeString } from "@lib/getTimeString";
+import { withPlural } from "@lib/withPlural";
+import classNames from "classnames";
+import React from "react";
+
+export interface SliceSummaryProps {
+  slice: OfferSlice;
+}
+
+export const SliceSummary: React.FC<SliceSummaryProps> = ({ slice }) => {
+  const firstSegment = slice.segments[0];
+  const lastSegment = slice.segments[slice.segments.length - 1];
+
+  const segmentsAndStops = slice.segments.flatMap((segment, index) => {
+    const stops = (segment.stops || []).map((stop) => stop.airport.iata_code!);
+    if (index == 0) {
+      // we use origins to identify the stop, so we ignore first segment since
+      // it's origin is already shown in the first `SliceSummaryTimeAndPlace`
+      return stops;
+    } else {
+      return [segment.origin.iata_code!, ...stops];
+    }
+  });
+
+  return (
+    <div className="slice-summary">
+      <SliceSummaryTimeAndPlace
+        time={getTimeString(firstSegment.departing_at)}
+        place={firstSegment.origin.iata_code!}
+        alignment="end"
+      />
+      <SliceStopsAndDurationOverview
+        duration={convertDurationToString(slice.duration!)}
+        stops={segmentsAndStops}
+      />
+      <SliceSummaryTimeAndPlace
+        time={getTimeString(lastSegment.arriving_at)}
+        place={lastSegment.destination.iata_code!}
+        alignment="start"
+      />
+    </div>
+  );
+};
+
+const SliceSummaryTimeAndPlace: React.FC<{
+  time: string;
+  place: string;
+  alignment: "start" | "end";
+}> = ({ time, place, alignment }) => (
+  <div
+    className={classNames(
+      "slice-summary__time-and-place",
+      `slice-summary__time-and-place--align-${alignment}`,
+    )}
+  >
+    <div className="slice-summary__time">{time}</div>
+    <div className="slice-summary__place">{place}</div>
+  </div>
+);
+
+const SliceStopsAndDurationOverview: React.FC<{
+  duration: string;
+  stops: string[];
+}> = ({ duration, stops }) => {
+  return (
+    <div>
+      <p className="slice-summary__duration">{duration}</p>
+
+      <div
+        className="slice-summary__flight-line-container"
+        style={{
+          gridTemplateColumns: `1fr repeat(${stops.length}, auto 1fr)`,
+        }}
+      >
+        <div className="slice-summary__flight-line" />
+
+        {stops.map((stop, index) => (
+          <React.Fragment key={stop + index}>
+            <div className="slice-summary__flight-line-dot" />
+            <div className="slice-summary__flight-line" />
+          </React.Fragment>
+        ))}
+      </div>
+
+      <p className="slice-summary__stops">
+        {stops.length === 0
+          ? "Direct"
+          : `${withPlural(stops.length, "stop", "stops")}, ${stops.join(", ")}`}
+      </p>
+    </div>
+  );
+};
