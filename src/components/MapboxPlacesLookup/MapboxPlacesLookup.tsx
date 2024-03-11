4ea7e9ecc394efc49ce2ae1ac459a259ceb423da
Igor de Paula <igordepaula1@gmail.com>
2024-03-11 14:07:48 +0000

    Adds missing style wrapper

--- src/components/MapboxPlacesLookup/MapboxPlacesLookup.tsx
+++ src/components/MapboxPlacesLookup/MapboxPlacesLookup.tsx
@@ -5,6 +5,7 @@ import {
   Place,
   getPlacesFromMapboxClient,
 } from "./lib/getPlacesFromMapboxClient";
+import { WithComponentStyles } from "@components/shared/WithComponentStyles";
 
 export interface MapboxPlacesLookupProps {
   mapboxPublicKey: string;
@@ -31,39 +32,41 @@ export const MapboxPlacesLookup: React.FC<MapboxPlacesLookupProps> = ({
   }, 300);
 
   return (
-    <div className="places-lookup">
-      <input
-        className={classNames("places-lookup-input", inputClassName)}
-        placeholder={placeholder}
-        type="text"
-        value={inputValue}
-        onChange={(e) => {
-          if (!shouldShowPopover) setShouldShowPopover(true);
-          setInputValue(e.target.value);
-          runLookup(e.target.value);
-        }}
-      />
-      {shouldShowPopover &&
-        inputValue.length > 0 &&
-        lookupResults.length > 0 && (
-          <div className="places-lookup-popover">
-            {lookupResults.map((place) => (
-              <button
-                className="places-lookup-popover__item"
-                key={place.shortName}
-                onClick={() => {
-                  setShouldShowPopover(false);
-                  onPlaceSelected(place);
-                  setInputValue(place.name);
-                }}
-              >
-                <span className="places-lookup-popover__icon-and-name-container">
-                  {place.name}
-                </span>
-              </button>
-            ))}
-          </div>
-        )}
-    </div>
+    <WithComponentStyles>
+      <div className="places-lookup">
+        <input
+          className={classNames("places-lookup-input", inputClassName)}
+          placeholder={placeholder}
+          type="text"
+          value={inputValue}
+          onChange={(e) => {
+            if (!shouldShowPopover) setShouldShowPopover(true);
+            setInputValue(e.target.value);
+            runLookup(e.target.value);
+          }}
+        />
+        {shouldShowPopover &&
+          inputValue.length > 0 &&
+          lookupResults.length > 0 && (
+            <div className="places-lookup-popover">
+              {lookupResults.map((place) => (
+                <button
+                  className="places-lookup-popover__item"
+                  key={place.shortName}
+                  onClick={() => {
+                    setShouldShowPopover(false);
+                    onPlaceSelected(place);
+                    setInputValue(place.name);
+                  }}
+                >
+                  <span className="places-lookup-popover__icon-and-name-container">
+                    {place.name}
+                  </span>
+                </button>
+              ))}
+            </div>
+          )}
+      </div>
+    </WithComponentStyles>
   );
 };
