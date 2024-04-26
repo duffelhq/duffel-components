fc786b43a262df079cafbc6d6a37db54f42a7212
Igor <igordepaula1@gmail.com>
2024-04-26 11:04:26 -0300

    Adds helper functions to sort and filter

---
+++ src/components/DuffelNGSView/lib/sort-ngs-rows-by-duration.ts
@@ -0,0 +1,25 @@
+import { NGSOfferRow } from "./group-offers-for-ngs-view";
+import { parse, toMinutes } from "duration-fns";
+
+type DurationSortDirection = "asc" | "desc";
+export type DurationSort = DurationSortDirection | null;
+
+export const sortNGSRowsByDuration = (
+  rows: NGSOfferRow[],
+  sortDuration: DurationSortDirection,
+): NGSOfferRow[] =>
+  rows.sort((aRow, bRow) => {
+    if (aRow.slice.duration === null || bRow.slice.duration === null) return 0;
+
+    const aParsedDuration = parse(aRow.slice.duration);
+    const bParsedDuration = parse(bRow.slice.duration);
+
+    const aDurationInMinutes = toMinutes(aParsedDuration);
+    const bDurationInMinutes = toMinutes(bParsedDuration);
+
+    if (sortDuration === "asc") {
+      return aDurationInMinutes - bDurationInMinutes;
+    } else {
+      return bDurationInMinutes - aDurationInMinutes;
+    }
+  });
