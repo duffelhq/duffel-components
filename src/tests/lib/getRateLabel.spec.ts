599419081c1550684462feaaa49bf9c5ac6e2f28
Jekri Preclaro Orlina <jekripreclaroorlina@gmail.com>
2024-06-05 12:32:39 +0100

    Test fallback case for card

--- src/tests/lib/getRateLabel.spec.ts
+++ src/tests/lib/getRateLabel.spec.ts
@@ -19,6 +19,16 @@ describe("getRateLabel", () => {
     expect(label).toEqual("Card payment at accommodation");
   });
 
+  test("returns the right label for a card rate with unknown type", () => {
+    const paymentMethod = "card";
+    const paymentType = "pay_later";
+
+    // @ts-expect-error assuming we receive a new payment type we have not identified yet
+    const label = getRateLabel(paymentMethod, paymentType);
+
+    expect(label).toEqual("Pay with card");
+  });
+
   test("returns the right label for a balance rate", () => {
     const paymentMethod = "balance";
     const paymentType = "pay_now";
