dd8456c47c9387fb18a540448254c9f3ca794341
Andreja Kogovsek <andreja.kogovsek1@gmail.com>
2024-10-18 14:22:09 +0100

    Storybook 8 (#338)
    Upgrades:
    - Yarn
    - Storybook
    - Sentry
    
    To bring us up to date, and help resolve security vulnerabilities.
    Next I'll release and then update all the examples to use the latest
    version (which should remove all the Sentry
    [vulnerabilties](https://github.com/duffelhq/duffel-components/security/dependabot)).
    
    <img width="910" alt="Screenshot 2024-10-18 at 11 14 06"
    src="https://github.com/user-attachments/assets/acbc3251-5a79-47ac-a3e8-93ad2e6b3c1f">

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -12,7 +12,6 @@ const decorators: [Decorator] = [
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
