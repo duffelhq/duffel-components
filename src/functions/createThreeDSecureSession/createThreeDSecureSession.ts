44a015e3d131b089197d4a59bac3e18a505e095a
Andreja Kogovsek <andreja.kogovsek1@gmail.com>
2025-01-09 09:24:23 +0000

    Allow cardholder present to be null

--- src/functions/createThreeDSecureSession/createThreeDSecureSession.ts
+++ src/functions/createThreeDSecureSession/createThreeDSecureSession.ts
@@ -23,7 +23,7 @@ const TEST_ENVIRONMENT_CONFIGURATION = {
  * @param cardId - The card ID used for the 3DS session.
  * @param resourceId - The resource (offer, order, order change) ID that the 3DS session is for.
  * @param services - Include all services that are being added, empty if no services are being added. This is required when services are also being purchased to ensure an accurate total amount to be authorised. If no services, it should be an empty array.
- * @param cardholderPresent - Whether the cardholder was present when the 3DS session was created. If you are collecting card details offline, for example an agent interface for entering card details received from the traveller over the phone, then you must specify the cardholder as not present
+ * @param cardholderPresent - Whether the cardholder was present when the 3DS session was created. If you are collecting card details offline, for example an agent interface for entering card details received from the traveller over the phone, then you must specify the cardholder as not present. This can be null if there is an exception.
  * @param exception - The name of the exception used to opt out of authenticating the payment with the card issuer
  */
 type CreateThreeDSecureSessionFn = (
@@ -31,7 +31,7 @@ type CreateThreeDSecureSessionFn = (
   cardId: string,
   resourceId: string,
   services: Array<{ id: string; quantity: number }>,
-  cardholderPresent: boolean,
+  cardholderPresent: boolean | null,
   exception?: string,
   environmentConfiguration?: Partial<typeof DEFAULT_ENVIRONMENT_CONFIGURATION>,
 ) => Promise<ThreeDSecureSession>;
