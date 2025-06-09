ae6581cace0393c76a8d6741f5a4042b5e21b38a
igorp1 <2934495+igorp1@users.noreply.github.com>
2025-06-09 15:02:19 -0300

    updates get label logic to highlight deposit rate type

--- src/components/Stays/lib/getRateLabel.ts
+++ src/components/Stays/lib/getRateLabel.ts
@@ -2,12 +2,16 @@ import { StaysRate } from "@duffel/api/types";
 
 export const getRateLabel = (
   paymentMethod: StaysRate["available_payment_methods"][number],
-  paymentType: StaysRate["payment_type"],
+  paymentType: StaysRate["payment_type"]
 ): string | undefined => {
-  if (paymentMethod === "card" && paymentType === "pay_now")
-    return "Pay now with card";
-  if (paymentMethod === "card" && paymentType === "guarantee")
-    return "Card payment at accommodation";
   if (paymentMethod === "balance") return "Pay now with Duffel Balance";
+  else {
+    // paymentMethod === "card" and...
+    if (paymentType === "pay_now") return "Pay now with card";
+    if (paymentType === "guarantee") return "Card payment at accommodation";
+    if (paymentType === "deposit") return "Pay deposit with card";
+  }
+
+  // fallback
   if (paymentMethod === "card") return "Pay with card";
 };
