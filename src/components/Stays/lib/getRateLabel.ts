11d9fa09d97cedc66128494cbc0882ce1d41cf57
igorp1 <2934495+igorp1@users.noreply.github.com>
2025-06-10 17:06:35 -0300

    simplifies IFs

--- src/components/Stays/lib/getRateLabel.ts
+++ src/components/Stays/lib/getRateLabel.ts
@@ -4,14 +4,22 @@ export const getRateLabel = (
   paymentMethod: StaysRate["available_payment_methods"][number],
   paymentType: StaysRate["payment_type"],
 ): string | undefined => {
-  if (paymentMethod === "balance") return "Pay now with Duffel Balance";
-  else {
-    // paymentMethod === "card" and...
-    if (paymentType === "pay_now") return "Pay now with card";
-    if (paymentType === "guarantee") return "Card payment at accommodation";
-    if (paymentType === "deposit") return "Pay deposit with card";
+  if (paymentMethod === "balance") {
+    return "Pay now with Duffel Balance";
+  }
+
+  if (paymentMethod === "card" && paymentType === "pay_now") {
+    return "Pay now with card";
+  }
+
+  if (paymentMethod === "card" && paymentType === "guarantee") {
+    return "Card payment at accommodation";
+  }
+
+  if (paymentMethod === "card" && paymentType === "deposit") {
+    return "Pay deposit with card";
   }
 
   // fallback
-  if (paymentMethod === "card") return "Pay with card";
+  return "Pay with card";
 };
